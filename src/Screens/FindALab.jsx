624a8e7b99592c8ca2a65697f531787c3f8f1645
Abdul <rahman778@gmail.com>
2024-07-15 19:47:13 +0530

    typography upgrade

--- src/Screens/FindALab.jsx
+++ src/Screens/FindALab.jsx
@@ -1,87 +1,134 @@
 import React, { useState } from "react";
-import { Button} from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
 import PatientServiceCenter from "../components/PatientServiceCenter";
-import { TextField } from "@mui/material"
+import { TextField } from "@mui/material";
 import { useAlert } from "react-alert";
 
 const FindALab = ({
-    goToNextStep,
-    zipCode,
-    setZipCode,
-    radius,
-    setRadius,
-    pcsData,
-    getPCSData,
-    selectedIndex,
-    setDrawFee,
-    setSelectedIndex,
+   goToNextStep,
+   zipCode,
+   setZipCode,
+   radius,
+   setRadius,
+   pcsData,
+   getPCSData,
+   selectedIndex,
+   setDrawFee,
+   setSelectedIndex,
 }) => {
+   const alert = useAlert();
 
-  const alert = useAlert();
-  
-  return (
-        <>
-          <h2 className="text-xl font-bold leading-7 text-gray-900 lg:text-2xl lg:tracking-tight">
+   return (
+      <>
+         <h2 className="text-xl font-bold leading-7 text-gray-900 lg:text-xl lg:tracking-tight">
             Select your Patient Service Center
-          </h2>
-          <div className="flex gap-4 mt-4  flex-col lg:flex-row">
+         </h2>
+         <div className="flex gap-4 mt-4  flex-col lg:flex-row">
             <TextField
-              label="Zip code"
-              type="number"
-              variant="outlined"
-              value={zipCode}
-              onChange={({ target: { value } }) => {
-                setZipCode(value);
-              }}
-              className="flex-initial lg:w-2/5 w-full"
+               label="Zip code"
+               variant="outlined"
+               value={zipCode}
+               onChange={({ target: { value } }) => {
+                  if (/^\d*$/.test(value)) {
+                     const numValue = Number(value);
+                     if (numValue > 0) {
+                        setZipCode(value);
+                     } else if (value === "") {
+                        setZipCode(value);
+                     }
+                  }
+               }}
+               className="flex-initial lg:w-2/5 w-full !font-sans"
+               InputLabelProps={{
+                  sx: {
+                     fontSize: "14px",
+                  },
+               }}
+               InputProps={{
+                  sx: {
+                     fontSize: "14px",
+                     "& .MuiInputBase-input": {
+                        fontSize: "14px",
+                     },
+                     "& .MuiInputBase-input::placeholder": {
+                        fontSize: "14px",
+                     },
+                  },
+               }}
             />
 
             <TextField
-              type="number"
-              label="Radius"
-              variant="outlined"
-              value={radius}
-              onChange={({ target: { value } }) => {
-                setRadius(value);
-              }}
-              className="flex-initial lg:w-2/5 w-full"
+               label="Radius"
+               variant="outlined"
+               value={radius}
+               onChange={({ target: { value } }) => {
+                  if (/^\d*$/.test(value)) {
+                     const numValue = Number(value);
+                     if (numValue > 0) {
+                        setRadius(value);
+                     } else if (value === "") {
+                        setRadius(value);
+                     }
+                  }
+               }}
+               className="flex-initial lg:w-2/5 w-full"
+               InputLabelProps={{
+                  sx: {
+                     fontSize: "14px",
+                  },
+               }}
+               InputProps={{
+                  sx: {
+                     fontSize: "14px",
+                     "& .MuiInputBase-input": {
+                        fontSize: "14px",
+                     },
+                     "& .MuiInputBase-input::placeholder": {
+                        fontSize: "14px",
+                     },
+                  },
+               }}
             />
 
             <Button
-              className="h-full align-middle select-none search-btn lg:w-1/5 lg:ms-6 w-full ms-0"
-              style={{}}
-              onClick={() => {
-                if (zipCode === "") {alert.show("Enter your zip code",{type: 'error',})}
-                else if (radius === "") alert.show("Enter radius to search", {type: 'error',});
-                else getPCSData({ zipCode, radius });
-              }}
+               className="h-full align-middle select-none search-btn lg:w-1/5 lg:ms-6 w-full ms-0"
+               style={{ fontSize: 14, fontWeight: "400", borderRadius: 3 }}
+               onClick={() => {
+                  if (zipCode === "") {
+                     alert.show("Enter your zip code", { type: "error" });
+                  } else if (radius === "")
+                     alert.show("Enter radius to search", { type: "error" });
+                  else getPCSData({ zipCode, radius });
+               }}
             >
-              Search
+               Search
             </Button>
-          </div>
-          <div className="mt-6 overflow-y-scroll wrap " >
+         </div>
+         <div className="mt-6 overflow-y-scroll wrap ">
             {pcsData?.map((PCSData, index) => (
-              <PatientServiceCenter
-                key={index}
-                onClick={() => {
-                  setSelectedIndex(index);
-                  setDrawFee(PCSData?.price);
-                }}
-                isSelected={index === selectedIndex}
-                PCSData={PCSData}
-              />
+               <PatientServiceCenter
+                  key={index}
+                  onClick={() => {
+                     setSelectedIndex(index);
+                     setDrawFee(PCSData?.price);
+                  }}
+                  isSelected={index === selectedIndex}
+                  PCSData={PCSData}
+               />
             ))}
-          </div>
-          <div className="flex flex-col items-end  mt-6">
+         </div>
+         <div className="flex flex-col items-end  mt-6">
             <Button
-             disabled={!(zipCode && radius && selectedIndex != '-1')}
-             onClick={goToNextStep}
-             className="w-40"
-             >Next</Button>
-          </div>
-        </>
-  );
+               disabled={!(zipCode && radius && selectedIndex != "-1")}
+               onClick={goToNextStep}
+               className="w-40"
+               style={{ fontSize: 14, fontWeight: "400", borderRadius: 3 }}
+            >
+               Next
+            </Button>
+         </div>
+      </>
+   );
 };
-
 
 export default FindALab;
--- src/components/PatientServiceCenter.jsx
+++ src/components/PatientServiceCenter.jsx
@@ -12,25 +12,25 @@ export default function PatientServiceCenter({PCSData, isSelected, onClick}) {
             className={`mt-6 border-2  drop-shadow-s rounded-lg p-4 box-size ${isSelected && "border-range-600"}`}>
 
             <div className="flex justify-between ">
-                <Typography variant="h5" className="w-1/2 text-range-400">{PCSData?.name}</Typography>
+                <Typography variant="h6" className="w-1/2 text-range-400">{PCSData?.name}</Typography>
                 <div className="flex">
-                    <Typography variant="h6" className="w-full">Draw fee ${PCSData.price}</Typography>
+                    <Typography variant="h6" className="w-full text-sm">Draw fee ${PCSData.price}</Typography>
                     <Tooltip title={`Please Note:
                                     *The Draw Fee is a pre-paid fee charged once for each
                                     visit to the Patient Service Center to cover the actual drawing of the patient's
                                     blood and or collection of their specimen(s). There will be no charges to the
                                     patient at the Patient Service Center.`}
                              className="ml-4">
-                        <InfoOutlinedIcon/>
+                        <InfoOutlinedIcon sx={{ fontSize: 18 }}/>
                     </Tooltip>
                 </div>
             </div>
             <Typography variant="paragraph"
-                        className="mt-6">{PCSData?.address1}, {PCSData?.address2}, {PCSData?.city}</Typography>
+                        className="mt-6 text-sm">{PCSData?.address1}, {PCSData?.address2}, {PCSData?.city}</Typography>
 
-            <Typography variant="paragraph" className="mt-2">Phone {PCSData?.phone}</Typography>
-            <Typography variant="paragraph" className="mt-6">Hours</Typography>
-            <div>{parse(PCSData.hours)}</div>
+            <Typography variant="paragraph" className="mt-2 text-sm">Phone {PCSData?.phone}</Typography>
+            <Typography variant="paragraph" className="mt-6 text-sm">Hours</Typography>
+            <div className=" text-sm">{parse(PCSData.hours)}</div>
 
         </Card>
     );
